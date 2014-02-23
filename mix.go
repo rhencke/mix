0ccb002377dbfa8b434e3150abe3ba10afbce821
Robert Hencke <robert.hencke@gmail.com>
2014-02-23 15:24:00 -0500

    Byte and Byte.String() tests

--- mix.go
+++ mix.go
@@ -1,1 +1,18 @@
 package mix
+
+import (
+	"fmt"
+)
+
+// A Byte represents one of 100 distinct values, on this particular MIX computer.
+type Byte byte
+
+func (b *Byte) String() string {
+	if *b > 99 {
+		return "??"
+	}
+	return fmt.Sprintf("%02d", byte(*b))
+}
+
+type Computer struct {
+}
--- mix_test.go
+++ mix_test.go
@@ -1,1 +1,22 @@
 package mix
+
+import (
+	"testing"
+)
+
+func TestByteString(t *testing.T) {
+	var tests = []struct {
+		value    Byte
+		expected string
+	}{
+		{0, "00"},
+		{99, "99"},
+		{100, "??"},
+	}
+
+	for i, v := range tests {
+		if v.value.String() != v.expected {
+			t.Errorf("test #%v, expected %v, got %v", i, v.expected, v.value.String())
+		}
+	}
+}
