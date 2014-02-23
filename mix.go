be1c23651b79534ab0392a4937d2c28ef35cbd0d
Robert Hencke <robert.hencke@gmail.com>
2014-02-23 16:09:56 -0500

    Sign, Address, JumpAddress, test cleanup

--- mix.go
+++ mix.go
@@ -7,12 +7,40 @@ import (
 // A Byte represents one of 100 distinct values, on this particular MIX computer.
 type Byte int8
 
-// A Word is composed of 6 fields, addressed from 0 to 5:
-//   [Sign, Byte, Byte, Byte, Byte, Byte]
+// The Sign of a field is either positive or negative.
+type Sign int8
 
-func (b *Byte) String() string {
-	if b == nil || *b > 99 || *b < 0 {
+// A Word is [Sign, Byte, Byte, Byte, Byte, Byte]
+type Word [6]Byte
+
+// An Address is [Sign, Byte, Byte]
+type Address [3]Byte
+
+// A JumpAddress is [Byte, Byte]
+type JumpAddress [2]Byte
+
+func (b Byte) String() string {
+	if b > 99 || b < 0 {
 		return "??"
 	}
-	return fmt.Sprintf("%02d", byte(*b))
+	return fmt.Sprintf("%02d", byte(b))
+}
+
+func (s Sign) String() string {
+	if s < 0 {
+		return "-"
+	}
+	return "+"
+}
+
+func (w Word) String() string {
+	return fmt.Sprint(Sign(w[0]), w[1], w[2], w[3], w[4], w[5])
+}
+
+func (a Address) String() string {
+	return fmt.Sprint(Sign(a[0]), a[1], a[2])
+}
+
+func (j JumpAddress) String() string {
+	return fmt.Sprint(j[0], j[1])
 }
--- mix_test.go
+++ mix_test.go
@@ -1,18 +1,28 @@
 package mix
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestByteString(t *testing.T) {
+func TestStrings(t *testing.T) {
 	var tests = []struct {
-		value    Byte
+		value    fmt.Stringer
 		expected string
 	}{
-		{-1, "??"},
-		{0, "00"},
-		{99, "99"},
-		{100, "??"},
+		0:  {Byte(-1), "??"},
+		1:  {Byte(0), "00"},
+		2:  {Byte(99), "99"},
+		3:  {Byte(100), "??"},
+		4:  {Sign(-50), "-"},
+		5:  {Sign(0), "+"},
+		6:  {Sign(50), "+"},
+		7:  {Word{0, 0, 0, 0, 0, 0}, "+ 00 00 00 00 00"},
+		8:  {Word{-1, 23, 45, 67, 89, 0}, "- 23 45 67 89 00"},
+		9:  {Address{0, 0, 0}, "+ 00 00"},
+		10: {Address{-1, 23, 45}, "- 23 45"},
+		11: {JumpAddress{0, 0}, "00 00"},
+		12: {JumpAddress{23, 45}, "23 45"},
 	}
 
 	for i, v := range tests {
@@ -20,12 +30,5 @@ func TestByteString(t *testing.T) {
 		if actual != v.expected {
 			t.Errorf("test #%v, expected %v, got %v", i, v.expected, actual)
 		}
-	}
-}
-
-func TestNilByteString(t *testing.T) {
-	actual := (*Byte)(nil).String()
-	if actual != "??" {
-		t.Error("expected ??, got", actual)
 	}
 }
